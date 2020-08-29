23924a8484e8b76907c8f094bd5c14a868b44ce2
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-08-29 16:41:15 +0900

    gye member info

--- src/Component/gye/GyeDetailsComponent.jsx
+++ src/Component/gye/GyeDetailsComponent.jsx
@@ -6,31 +6,41 @@ class GyeDetailsComponent extends React.Component {
         super(props);
 
         this.state = {
-            gye : []
+            gye : [],
+            members : []
         };
     }
 
     componentDidMount() {
         GyeService.getGyeDetails(this.props.id)
         .then(response => {
+            
             this.setState({gye: response.data});
+            this.setState({members: this.state.gye.members});
+  
+            console.log(this.state.gye);
+            console.log(this.state.members);
         })
         .catch(error => {
 
-        })
+        });
     }
 
     render() {
         let gye = this.state.gye;
-        
+        const memberList = this.state.members.map((member, index) =>
+            <li key={index}>{member.email} : {member.name} </li>)
         return (
              <div>
-                 <h1>{gye.title}</h1>
-                 <h2>Gye Mater : {gye.master}</h2>
-                 <p>Type : {gye.type}</p>
-                 <p>Target Money : {gye.targetMoney}</p>
-                 <p>Total Member : {gye.totalMember}</p>
-                 <p>Period : {gye.period}month</p>
+                <h1>{gye.title}</h1>
+                <h2>Gye Mater : {gye.master}</h2>
+                <p>Type : {gye.type}</p>
+                <p>Target Money : {gye.targetMoney}</p>
+                <p>Total Member : {gye.totalMember}</p>
+                <p>Period : {gye.period}month</p>
+
+                <h2>Member List</h2>
+                <ul>{memberList}</ul> 
                  
              </div>
         );

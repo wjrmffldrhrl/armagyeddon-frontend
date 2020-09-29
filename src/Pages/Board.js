c3058a9764ba3fac146a31fbe5559333e18f8760
2-juhyun-2 <juhyun9086@naver.com>
2020-09-29 12:59:37 +0900

    0929

--- src/Pages/Board.js
+++ src/Pages/Board.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import BoardComponent from '../Component/board/BoardComponent';
+import styles from '../static/css/board.css';
 
 class Board extends React.Component {
     render() {
         return(
-            <div>
-                아마곗돈 게시판이다 이놈아
+            <div className="board_card">
+                    <div className="board_title">
+                    <h1>Armagyeddon Board</h1>
+                    </div>
+
+                    <div className="board_container">
+                    <BoardComponent></BoardComponent>
+                    </div>
             </div>
         );
     }

493b47183989c3ebd5becea69da35ecd3d5017c8
minhyeok3924 <minhyeok3924@naver.com>
2020-10-12 11:22:14 +0900

    delete  bad code + edit gyecreate

--- src/Component/gye/GyeManagementComponent.jsx
+++ src/Component/gye/GyeManagementComponent.jsx
@@ -74,56 +74,6 @@ class GyeManagementComponent extends React.Component {
 
 
         //낙찰계 수금
-        let collectInterestMoney = [
-            //공통적으로 내는 돈 + 이자
-            //3명일때 1번째 사람
-            // (gye.targetMoney / gye.totalMember) + ((gye.targetMoney * (gye.interest-1) / 100) / 12 * gye.period)
-
-            //3명일때 2번째 사람
-            // (gye.targetMoney / gye.totalMember)
-
-            //3명일때 3번째 사람
-            // (gye.targetMoney / gye.totalMember) - ((gye.targetMoney * (gye.interest) / 100) / 12 * gye.period)
-
-            //5명일때 1번째 사람
-            // (gye.targetMoney / gye.totalMember) + ((gye.targetMoney * (gye.interest-2) / 100) / 12 * gye.period)
-
-            //5명일때 2번째 사람
-            // (gye.targetMoney / gye.totalMember) + ((gye.targetMoney * (gye.interest / 2 -1) / 100) / 12 * gye.period)
-
-            //5명일때 3번째 사람
-            // (gye.targetMoney / gye.totalMember)
-
-            //5명일때 4번째 사람
-            // (gye.targetMoney / gye.totalMember) - ((gye.targetMoney * (gye.interest / 2) / 100) / 12 * gye.period)
-
-            //5명일때 5번째 사람
-            // (gye.targetMoney / gye.totalMember) - ((gye.targetMoney * (gye.interest) / 100) / 12 * gye.period)
-
-
-            //7명일때 1번째 사람
-            // (gye.targetMoney / gye.totalMember) + ((gye.targetMoney * (gye.interest - 1 /3 ) / 100) / 12 * gye.period)
-
-            //7명일때 2번째 사람
-            // (gye.targetMoney / gye.totalMember) + ((gye.targetMoney * (2 * (gye.interest) / 3 - 1 /2 ) / 100) / 12 * gye.period)
-
-            //7명일때 3번째 사람
-            //  (gye.targetMoney / gye.totalMember) + ((gye.targetMoney * (gye.interest / 3 -1) / 100) / 12 * gye.period)
-
-            //7명일때 4번째 사람
-            // (gye.targetMoney / gye.totalMember)
-
-            //7명일때 5번째 사람
-            // (gye.targetMoney / gye.totalMember) - ((gye.targetMoney * (gye.interest / 3) / 100) / 12 * gye.period)
-
-            //7명일때 6번째 사람
-            // (gye.targetMoney / gye.totalMember) - ((gye.targetMoney * (2 * (gye.interest / 3)) / 100) / 12 * gye.period)
-
-            //7명일때 7번째 사람
-            // (gye.targetMoney / gye.totalMember) - ((gye.targetMoney * (gye.interest) / 100) / 12 * gye.period)
-
-            // 인원수 // 이자율 // 해당인원의 금액
-        ]
 
 
         for (let i = 0; i < this.state.members.length; i++) {

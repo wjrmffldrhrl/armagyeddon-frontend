71d7a45fdbd28b102e180bc48db4584e327761d2
2-juhyun-2 <juhyun9086@naver.com>
2020-10-13 15:34:03 +0900

    1013

--- src/Component/login/PWSearchComponent.jsx
+++ src/Component/login/PWSearchComponent.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import axios from 'axios';
+import AuthenticationService from '../../services/AuthenticationService';
 import styles from '../../static/css/login.css';
 
 class PWSearchComponent extends React.Component {
+
+
+    executeJwtAuthenticationService(email, password) {
+
+        return axios.post('/authenticate', {
+            email,
+            password
+        })
+    }
+
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            email: localStorage.getItem("authenticatedUser") || '',
+            password: '',
+            token: localStorage.getItem("token") || '',
+            hasSearchFailed: false,
+            showSuccessMessage: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.searchClicked = this.searchClicked.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState(
+            {
+                [event.target.name]
+                :event.target.value
+            }
+        )
+    }
+
+    searchClicked() {
+
+        AuthenticationService
+        // get email, password at the form
+        .executeJwtAuthenticationService(this.state.email, this.state.password)
+        .then((response) => {
+            console.log(response);
+            this.setState({
+                token: response.data.token
+            });
+
+        AuthenticationService.registerSuccessfulLoginForJwt(this.state.email,this.state.token);
+        this.props.history.push(`/pwsearch`);
+        this.props.userStateChange(AuthenticationService.isUserLoggedIn());
+        }).catch( () =>{
+            this.setState({showSuccessMessage:false})
+            this.setState({hasLoginFailed:true})
+        
+        }) 
+    }
+    
+
     render() {
         return(
 
@@ -14,67 +71,33 @@ class PWSearchComponent extends React.Component {
                             </div>
                         </div>
 
-                        <div className="d-flex justify-content-center form_container" class="static">
-                            <label>
-                            <div className="search_pw_id">
-                            <h5>ID</h5>
-                            <input type='text' maxLength='15' name='search_pw_id'/>
-                    
-                            </div>
-                            </label>
-                            
-                            <label >
-                            
-                            <div className="search_pw_email">
-                            <h5>Email</h5>
-                            <input type='text' maxLength='15' name='search_pw_email1'/>
-
-                            <div id='search_id_email_div'>
-                            @
-                            <input type='text' maxLength='15' name='search_pw_write_email'/>
-                            </div>
-                            </div>
-                            <div className="btn_pwsubmit">
-                             <button >조회하기</button>
+                            {/* 이메일 입력창 */}
+                            <div>
+                                Email
+                            <input type="email" name="email" className="form-control input_user" 
+                                value={this.state.email} onChange={this.handleChange} placeholder="Email address"/>
+                               
+                                {/* 조회하기 버튼 */}
+                                <div className="d-flex justify-content-center mt-3 login_container">
+                                    <button className="btn login_btn" onClick={this.searchClicked}>조회하기</button>
+                                </div>
                             </div>
 
-                         
-                            
-                            </label>
-                        </div>
-                
+
+                            {/* 비밀번호 입력창 */}
+
+                            <div>
+                                비밀번호
+                            <input type="password" name="password" className="form-control input_pass" 
+                                value={this.state.password} onChange={this.handleChange} placeholder="password"/>
+                            </div>
+
                         
+
                     </div>
                 </div>
             </div>
-            
-
-
-            // <div className="Search_div">
-            //     <h4> 비밀번호 찾기 </h4>
-
-            //     <div className="search_pw_id">
-            //         <h5>ID</h5>
-            //         <input type='text' maxLength='15' name='search_pw_id'/>
-                    
-            //     </div>
-
-                // <div className="search_pw_email">
-                //     <h5>Email</h5>
-                //     <input type='text' maxLength='15' name='search_pw_email1'/>
-
-                //     <div id='search_id_email_div'>
-                //         @
-                //         <input type='text' maxLength='15' name='search_pw_write_email'/>
-                //     </div>
-                // </div>
-
-                // <div className="search_pw_submit">
-                //     <input type='button' value='조회하기' name='search_pw_submit'/>                    
-
-                // </div>
-
-            // </div>
+         
         );
     }
 }
